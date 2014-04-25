57412e36d0027088ee077c4c8994efd74cbd6c59
Tiarella <tiarella84@gmail.com>
2014-04-25 20:08:41 +0200

    created mvvm test

---
+++ MVVMtest/MVVMtest/Person.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MVVMtest
+{
+    // ---------- MODEL ---------------------------------------
+    public class Person
+    {
+        public string Name { get; set; }
+        public int Alter { get; set; }
+
+        public static Person CreatePerson()
+        {
+            return new Person { Name = "Franz", Alter = 45 };
+        }
+    }
+}
---
+++ MVVMtest/MVVMtest/PersonViewModel.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+
+namespace MVVMtest
+{
+    // ---------- VIEWMODEL ---------------------------------------
+    public class PersonViewModel : INotifyPropertyChanged
+    {
+        private Person _Model;
+
+        //  Konstruktor: Erstelle neue Person aus Model-Klasse
+
+        public PersonViewModel(Person model)
+        {
+            _Model = model;
+            _Name = _Model.Name;
+            _Alter = _Model.Alter;
+        }
+
+        // Schnittstellen-Ereignis
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected internal void OnPropertyChanged(string propertyname)
+        {
+            if (PropertyChanged != null)
+                PropertyChanged(this, new PropertyChangedEventArgs(propertyname));
+        }
+
+        // Eigenschaften
+
+        private string _Name;
+        public string Name
+        {
+            get { return _Name; }
+            set
+            {
+                if (_Name == value) return;
+                _Name = value;
+                OnPropertyChanged("Name");
+            }
+        }
+
+        private int _Alter;
+        public int Alter
+        {
+            get { return _Alter; }
+            set
+            {
+                if (_Alter == value) return;
+                _Alter = value;
+                OnPropertyChanged("Alter");
+            }
+        }
+    }
+}
